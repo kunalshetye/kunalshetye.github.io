9f4a1b19b88308698b603c5dc4691773e5188289
Kunal Shetye <kshetye@sdl.com>
2016-06-09 15:40:22 +0200

    Fixed the publishing queue extension

--- tm/Publishing.Queue.user.js
+++ tm/Publishing.Queue.user.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name         Publish Queue Enhancements
+// @namespace    http://kunalshetye.github.io/
+// @version      0.1
+// @description  Make the Publishing Queue nice
+// @author       Kunal Shetye
+// @match        http://*/WebUI/Editors/CME/Views/Popups/PublishQueue/PublishQueue.aspx
+// @match        http://*/WebUI/Editors/CME/Views/Popups/PublishQueue/PublishQueue.aspx
+// @grant        none
+// @require      http://kunalshetye.github.io/js/jquery.js
+// @noframes     1
+// ==/UserScript==
+
 $kj(function(){
 	$kj("#ButtonContainer").prepend('<input type="checkbox" id="autorefresh" name="autorefresh"><label for="autorefresh">Auto Refresh</label>');
 	setInterval(AutoRefresh,5000);
