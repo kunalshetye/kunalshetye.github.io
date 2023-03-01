751bac84a668464b70aa976a4cf834ce073ed2c1
Kunal Shetye <kunal.shetye@gmail.com>
2023-03-01 12:34:09 +0100

    testing web exp results

---
+++ web-exp-results/Abandoned Cart Experiment_ Experiment Results - Optimizely_files/geo4.js
@@ -0,0 +1,17 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "AMSTERDAM",
+      'continent': "EU",
+      'country': "NL",
+      'region': "",
+      'dma': ""
+    },
+    'ip':"45.83.240.235"
+  }]);
+})
+//
+()
+
+;
